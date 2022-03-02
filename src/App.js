6835c02eddc8bed8145cdfb4f428b1ecef3d4a2b
miuhuru <zermoun.yasmine@gmail.com>
2022-03-02 19:03:55 +0100

    added three components for the main section of the cv builder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from "react";
+import GeneralInfo from "./components/GeneralInfo";
+import EducExp from "./components/EducExp"
+import WorkExp from "./components/WorkExp"
 
-function App() {
-  return (
-    <div>
-      <p></p>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GeneralInfo />
+        <EducExp />
+        <WorkExp />
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
