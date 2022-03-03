dd17818a3e9280b6ac0d94899646697be17e4714
miuhuru <zermoun.yasmine@gmail.com>
2022-03-03 21:42:14 +0100

    generalinfo component is functional

--- src/App.js
+++ src/App.js
@@ -4,46 +4,14 @@ import GeneralInfo from './components/GeneralInfo';
 import Header from './components/Header';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.makeDiv = this.makeDiv.bind(this)
-
-    this.state = {
-        user: {
-            firstName: "",
-            lastName: "",
-            Email: "",
-            phoneNumber: ""                
-        },
+     render() {
+      return (
+        <div className="App">
+          <Header />
+          <GeneralInfo />
+        </div>
+      )
     }
   }
-
-  submitInput = e => {
-    e.preventDefault()
-    this.setState({
-      user: {
-        firstName: "aaaaaaa",
-        lastName: "nooo"
-      }
-    })
-  }
-
-  makeDiv = (e) => {
-    e.preventDefault()
-    this.setState(
-      console.log("yooo!!")
-  )
-  }
-
-    render() {
-        const { user, makeDiv } = this.state;
-    return (
-        <div className="App">
-            <Header />
-        <GeneralInfo user={user} makeDiv={makeDiv} />
-      </div>
-    )
-  }
-}
 
 export default App;
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,32 +1,73 @@
-import React, { Component } from "react";
+import React from "react";
 
-class GeneralInfo extends React.Component {
+export default class GeneralInfo extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: {
+                firstName: "",
+                lastName: "",
+                Email: "",
+                phoneNumber: ""
+            },
+        }
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            user: {
+                firstName: e.target.value
+            }
+        })
+    }
+
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.setState({
+            user: {
+                firstName: "",
+                lastName: "",
+                Email: "",
+                phoneNumber: ""
+            }
+        });
+        console.log(this.state.user.firstName)
+    }
+
+
     render() {
         return (
             <div className="generalInfo">
-                <form className="form">
-                    <label> <p>First Name: </p>
-                        <input type="text" id="firstNameInput" />
+                <form className="form" onClick={this.handleSubmit}>
+                    <label>
+                        <p>First Name: </p>
+                        <input type="text" id="firstNameInput" value={this.state.user.firstName} onChange={this.handleInputChange} />
                     </label>
-                    <label><p>Last Name: </p>
-                        <input type="text" id="lastNameInput" />
-                        </label>
-                    <label> <p>Email: </p>
-                        <input type="text" id="firstNameInput" />
+
+                    <label>
+                        <p>Last Name: </p>
+                        <input type="text" id="lastNameInput" value={this.state.user.lastName} onChange={this.handleInputChange}/>
                     </label>
-                    <label> <p>Phone Number:</p>
-                        <input type="text" id="firstNameInput" />
+
+                    <label>
+                        <p>Email: </p>
+                        <input type="text" id="firstNameInput" value={this.state.user.Email} onChange={this.handleInputChange}/>
                     </label>
+
+                    <label>
+                        <p>Phone Number:</p>
+                        <input type="text" id="firstNameInput" value={this.state.user.phoneNumber} onChange={this.handleInputChange} />
+                    </label>
+
                     <div className="buttonContainer">
-                        <button type="submit" onClick={this.props.makeDiv}>
-                        Save
-                    </button>
+                        <button type="submit"> Save </button>
                     </div>
-                    {this.props.user.firstName}
                 </form>
             </div>
         )
     }
 }
-export default GeneralInfo
+
 
