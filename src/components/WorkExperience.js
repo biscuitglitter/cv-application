9626d83bf7b61fc743d215e34221ec02127a2197
miuhuru <zermoun.yasmine@gmail.com>
2022-03-06 15:47:03 +0100

    refactored workExperience component

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,71 +1,53 @@
-import React from "react";
-import './styles/WorkExperience.css'
+import React, { Component } from 'react';
 
-
-export default class Testie extends React.Component {
+export default class WorkExperience extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			listOfJobs: [
-                {
-                    action: {
-                        description: "",
-                        date: ""
-                } },
+				{ jobTitle: "systems engineer", company: "ibm", fromDate: "11-2021", untilDate: "04-2022", description: "very good and enriching experience" },
 			],
-			newJob: "",
-		};
+			newJob: { jobTitle: "", company: "", fromDate: "", untilDate: "", description: "" }
+		}
 	}
 
-	updateValue = e => {
-        this.setState({
-            newJob: {
-                action: {
-                    [e.target.name]: e.target.value,
-            }
-        } });
-	};
-
-	newJob = () => {
-		this.setState({
-			listOfJobs: [
-				...this.state.listOfJobs,
-                {
-                    action: {
-                        description: this.state.description,
-                        date: this.state.date
-                } },
-			],
-		});
-	};
-
-	jobRows = () =>
-		this.state.listOfJobs.map((item) => (
-			<jobRows key={item.action} item={item} />
-		));
-
-	render = () => (
-		<div className="container">
-			<div className="row">
-				<div className="col-12">
-					<input
-                        className="form-control"
-                        name="description"
-						value={this.state.description}
-						onChange={this.updateValue}
-                    />
-                    <input
-                        className="form-control"
-                        name="date"
-						value={this.state.date}
-						onChange={this.updateValue}
-					/>
-					<button className="btn btn-primary" onClick={this.newJob}>
-						Add
-					</button>
-				</div>				
+		updateValue = (event) => {
+			this.setState(
+				{
+					newJob: {
+						...this.state.newJob,
+						[event.target.name]: event.target.value
+					}
+				},
+			);
+		};
+	
+		newJob = () => {
+			this.setState({
+				listOfJobs: [
+					...this.state.listOfJobs,
+					this.state.newJob,
+				],
+			});
+		};
+	
+		jobList = () =>
+			this.state.listOfJobs.map((item) => (
+				console.log( `${item.description}` )
+			));
+		
+		render = () => (
+			<div className="container">
+				<div className="row">
+		<label><input type="text" placeholder="job" name="jobTitle" value={this.state.job} onChange={this.updateValue} /></label>
+		<label><input type="text" placeholder="company" name="company" value={this.state.company} onChange={this.updateValue} /></label>
+		<label><input type="text" placeholder="fromDate" name="fromDate" value={this.state.fromDate} onChange={this.updateValue} /></label>
+		<label><input type="text" placeholder="untilDate" name="untilDate" value={this.state.untilDate} onChange={this.updateValue} /></label>
+		<label><input type="text" placeholder="description" name="description" value={this.state.description} onChange={this.updateValue} /></label>
+					<button onClick={this.newJob}> Add	</button>
+					<button onClick={this.jobList}> Joblist	</button>
+				</div>
 			</div>
-		</div>
-	); 
-}
+		);
+	}
