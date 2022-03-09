bfcf88bec2f5fde4d23f303d1a8bde7fb1339aaa
miuhuru <zermoun.yasmine@gmail.com>
2022-03-09 17:25:30 +0100

    handleEdit populates the inputs with the right data

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -16,6 +16,7 @@ export default class WorkExperience extends Component {
 		this.state = {
 			listOfJobs: [],
 			newJob: { jobTitle: "", company: "", sinceDate: "", untilDate: "", description: "", id: uniqid() },
+			errors: {}
 		}
 
 	}
@@ -31,13 +32,29 @@ export default class WorkExperience extends Component {
 		);
 	};
 
-	addNewJob = (event) => {
+		handleEdit = (id) => event => {
+		const indexJob = this.state.listOfJobs.findIndex(item => item.id === id);
+		const targetedJob = this.state.listOfJobs[indexJob]
+		this.setState(
+			() => {
+				this.inputText1.current.value = targetedJob.jobTitle
+				this.inputText2.current.value = targetedJob.company
+				this.inputText3.current.value = targetedJob.sinceDate
+				this.inputText4.current.value = targetedJob.untilDate
+				this.inputText5.current.value = targetedJob.description
+			}
+		)
+	}
+
+
+	onSubmitJob = (event) => {
 		event.preventDefault()
 		this.setState({
 			listOfJobs: [
 				...this.state.listOfJobs,
 				this.state.newJob,
 			],
+			newJob: { jobTitle: "", company: "", sinceDate: "", untilDate: "", description: "", id: uniqid() }
 		},
 			() => {
 				this.inputText1.current.value = ""
@@ -61,16 +78,16 @@ export default class WorkExperience extends Component {
 		<div className="workExp">
 			<form className="form">
 				<h1>Work Experience</h1>
-				<label><input type="text" ref={this.inputText1} placeholder="job title" name="jobTitle" value={this.state.job} onChange={this.handleChange} required /></label>
-				<label><input type="text" ref={this.inputText2} placeholder="company" name="company" value={this.state.company} onChange={this.handleChange}  /></label>
-				<label><input type="text" ref={this.inputText3} placeholder="since" name="sinceDate" value={this.state.fromDate} onChange={this.handleChange} /></label>
-				<label><input type="text" ref={this.inputText4} placeholder="until" name="untilDate" value={this.state.untilDate} onChange={this.handleChange} /></label>
+				<label><input type="text" ref={this.inputText1} placeholder="job title" name="jobTitle" value={this.state.job} onChange={this.handleChange} /></label>
+				<label><input type="text" ref={this.inputText2} placeholder="company" name="company" value={this.state.company} onChange={this.handleChange}   /></label>
+				<label><input type="text" ref={this.inputText3} placeholder="since" name="sinceDate" value={this.state.fromDate} onChange={this.handleChange}  /></label>
+				<label><input type="text" ref={this.inputText4} placeholder="until" name="untilDate" value={this.state.untilDate} onChange={this.handleChange}  /></label>
 				<label><input type="text" ref={this.inputText5} placeholder="description" name="description" value={this.state.description} onChange={this.handleChange} /></label>
-				<button type="submit" onClick={this.addNewJob}> Add </button>
+				<button type="submit" onClick={this.onSubmitJob}> Add </button>
 			</form>
 			<div className="listAllJobs">
 				{this.state.listOfJobs.map((item, index) => {
-					return <div className="aNewJobRendered" key={item.id} >
+					return <div className="aNewJobRendered" key={item.id}>
 						<div className="workExpContainer">
 							<div className="topContainer">
 								<div className="topLeft">
@@ -80,20 +97,20 @@ export default class WorkExperience extends Component {
 
 								<div className="topRight">
 									<div className="buttonContainer">
-								<button className="closeButton" onClick={this.handleDelete(item)} key={index}> X </button>
+										<button className="closeButton" onClick={this.handleDelete(item)} key={index}> X </button>
 									</div>
 									<div className="datesContainer"> 
 									<div className="sinceDate"> {item.sinceDate} </div>
 									<div className="betweenDates"> - </div>
 									<div className="untilDate"> {item.untilDate} </div> </div>
 								</div>
-
 							</div>
 							<div className="middle">
 								<div className="description"> {item.description} </div>
 							</div>
 						</div>
-					</div>;
+						<button className="editButton" onClick={this.handleEdit(item.id)} id={item.id}> Edit </button>
+					</div>
 				})}
 			</div>
 		</div>
